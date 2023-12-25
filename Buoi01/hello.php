0ef502e3d6c7bfb67a5dcea3f7e031efdd57267b
dinhthituyetchinh <fengdinh@gmail.com>
2023-12-25 12:30:13 +0700

    first commit

---
+++ Buoi01/hello.php
@@ -0,0 +1,4 @@
+<?php
+print 'Hello world';
+print 'Đinh Thị Tuyết Chinh';
+?>
---
+++ Buoi01/html_php.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        <?= 
+        "alert('Hello');";
+        ?>
+    </script>
+</head>
+<body>
+    <h2>Đinh Thị Tuyết Chinh</h2>
+    <?php echo "Hello world ";?>
+
+    <?php
+        print "<h1>Hello</h1>";
+    ?>
+
+    <?="<i>Tuyet Chinh </i> " ?>
+    <p  <?='style ="color:red"' ?>> This is red text</p>
+
+    <?= '<p>' ?> This is text from Html <?= '<p>' ?>
+</body>
+</html>
